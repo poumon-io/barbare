e085a5ba8508c7470f6eb551d727b9b3ce6c5e30
Caedrik <caedrikbl@gmail.com>
2026-03-24 09:46:28 -0400

    icons

--- dist/barbare.js
+++ dist/barbare.js
@@ -11566,7 +11566,7 @@ Please report this to https://github.com/markedjs/marked.`,e){let r="<p>An error
   // ==================== MAPPING COMPLET DES TYPES D'ACTIVITÉS (0 à 9) ====================
   const ACTIVITY_TYPES = {
     0: {
-      innerIcon: `<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path>`,
+      innerIcon: `<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><line x1="19" x2="19" y1="8" y2="14"/><line x1="22" x2="16" y1="11" y2="11"/>`,
       action: "S'est inscrit(e) sur le forum",
       bg: "from-emerald-500 to-teal-600",
     },
